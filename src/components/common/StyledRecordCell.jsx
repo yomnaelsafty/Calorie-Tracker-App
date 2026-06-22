b2451ae5ba94538a370ad4599a8d6ea836f7b53f
yomnaelsafty <140314499+yomnaelsafty@users.noreply.github.com>
2026-06-22 20:49:54 +0300

    Update StyledRecordCell.jsx

--- src/components/common/StyledRecordCell.jsx
+++ src/components/common/StyledRecordCell.jsx
@@ -1,4 +1,4 @@
-import styles from "./StyledRecordCell.module.CSS";
+import styles from "./StyledRecordCell.module.css";
 
 function StyledRecordCell(props) {
   return <div className={styles["styled-record-cell"]}>{props.children}</div>;
