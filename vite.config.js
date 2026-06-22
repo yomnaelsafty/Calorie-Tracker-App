6f8a9a0715b30e0a9a5e5367c77c564f08aaa724
yomnaelsafty <140314499+yomnaelsafty@users.noreply.github.com>
2026-06-22 19:43:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react( )],
-  base: "/Calorie-Tracker-App/", 
-  server: {
-    open: true,
-  },
-});
+  plugins: [react()],
+  base: '/Calorie-Tracker-App/',  
+})
